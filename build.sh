52b22b09c08bc3d2a43305c6dff7fa9fc3699b74
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2021-08-09 15:13:01 +0100

    Updated minimal build environment

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 zipfile="js13k.zip"
 buildpath="tmpbuild"
+jscat="${buildpath}/min.js"
+indexcat="${buildpath}/index.html"
 
 # Create clean build folder
 rm -Rf "${buildpath}" >/dev/null 2>&1
@@ -9,21 +11,51 @@ rm -Rf "${zipfile}" >/dev/null 2>&1
 mkdir "${buildpath}"
 
 # Concatenate the JS files
+touch "${jscat}" >/dev/null 2>&1
+for file in "main.js"
+do
+  cat "${file}" >> "${jscat}"
+done
 
 # Add the index header
+echo -n '<!DOCTYPE html><html><head><meta charset="utf-8"/><meta http-equiv="Content-Type" content="text/html;charset=utf-8"/><title>JS13k 2021</title><style>' > "${indexcat}"
 
 # Inject the concatenated and minified CSS files
+for file in "main.css"
+do
+  yui-compressor "${file}" >> "${indexcat}"
+done
 
 # Add on the rest of the index file
+echo -n '</style><script type="text/javascript">' >> "${indexcat}"
 
 # Inject the closure-ised and minified JS
+./closeyoureyes.sh "${jscat}" >> "${indexcat}"
 
 # Add on the rest of the index file
+echo -n '</script><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/></head><body></body></html>' >> "${indexcat}"
 
 # Remove the minified JS
+rm "${jscat}" >/dev/null 2>&1
 
 # Zip everything up
+zip -j "${zipfile}" "${buildpath}"/*
 
 # Re-Zip with advzip to save a bit more
+advzip -i 200 -k -z -4 "${zipfile}"
 
 # Determine file sizes and compression
+unzip -lv "${zipfile}"
+stat "${zipfile}"
+
+zipsize=`stat -c %s "${zipfile}"`
+maxsize=$((13*1024))
+bytesleft=$((${maxsize}-${zipsize}))
+percent=$((200*${zipsize}/${maxsize} % 2 + 100*${zipsize}/${maxsize}))
+
+if [ ${bytesleft} -ge 0 ]
+then
+  echo "YAY ${percent}% used - it fits with ${bytesleft} bytes spare"
+else
+  echo "OH NO ${percent}% used - it's gone ovey by "$((0-${bytesleft}))" bytes"
+fi
