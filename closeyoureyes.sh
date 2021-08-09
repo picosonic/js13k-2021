60fdcc40da68bf2be1b7b1a00892a408a88f5222
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2021-08-09 12:08:24 +0100

    Added build files

---
+++ closeyoureyes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cpath="closure-compiler"
+
+# Check if we need closure locally
+if [ ! -d "${cpath}" ]
+then
+  echo "Follow instructions at https://developers.google.com/closure/compiler/docs/gettingstarted_app"
+  echo "Downloading the latest JAR from https://mvnrepository.com/artifact/com.google.javascript/closure-compiler"
+  exit
+fi
+
+# Check for input script name
+if [ $# -ne 1 ]
+then
+  echo "Specify script on command line"
+  exit
+fi
+
+# Find out the latest jar filename
+compiler=`ls -rt ${cpath}/closure-compiler-*.jar`
+
+# Use first parameter as JS file, write to stdout with ADVANCED compilation level
+java -jar "${compiler}" --compilation_level ADVANCED --js "$1"
