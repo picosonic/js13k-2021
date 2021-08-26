e593d40534ded9a34e2593595f47b2dcbe66bd57
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2021-08-27 00:17:36 +0100

    Renamed canvas

--- build.sh
+++ build.sh
@@ -51,7 +51,7 @@ echo -n '</style><script type="text/javascript">' >> "${indexcat}"
 ./closeyoureyes.sh "${jscat}" >> "${indexcat}"
 
 # Add on the rest of the index file
-echo -n '</script><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/></head><body><canvas id="canvas" width="1280" height="720"></canvas><canvas id="hud" width="1280" height="720"></canvas><canvas id="hud2" width="1280" height="720"></canvas></body></html>' >> "${indexcat}"
+echo -n '</script><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/></head><body><canvas id="threedee" width="1280" height="720"></canvas><canvas id="hud" width="1280" height="720"></canvas><canvas id="hud2" width="1280" height="720"></canvas></body></html>' >> "${indexcat}"
 
 # Remove the minified JS
 rm "${jscat}" >/dev/null 2>&1
